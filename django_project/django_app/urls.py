f4f78fa433e65657c9fda700443990ea84a34df8
zhurb21 <nickzhurbich23@gmail.com>
2020-10-16 00:06:59 +0700

    4 commit

--- django_project/django_app/urls.py
+++ django_project/django_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homePage),
+    path('', views.homePage, name = "home"),
+    path('register/', views.registerPage, name = "register"),
+    path('login/', views.loginPage, name = "login"),
 ]
--- django_project/django_app/views.py
+++ django_project/django_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from .models import *
+from .forms import CreateUserForm
 
 # Create your views here.
 def homePage(request):
@@ -12,3 +13,22 @@ def homePage(request):
 
     context = {'asias' : asias, 'americas': americas, 'africas' : africas, 'europes' : europes}
     return render(request, 'django_app/home.html', context)
+
+def registerPage(request):
+    form = CreateUserForm()
+
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid:
+            form.save()
+        return redirect('login')
+
+    context = {'form' : form}
+
+    return render(request, 'django_app/register.html', context)
+
+def loginPage(request):
+    context = {}
+
+    return render(request, 'django_app/login.html', context)
